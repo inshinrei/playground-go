6ee597df3b947fdb1ba25a2960db94b071ecbda8
tokkiumai <kiwidancebad@gmail.com>
2024-04-25 21:34:22 +0300

    n. read status

--- begin/hello.go
+++ begin/hello.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
-
-func getName() string {
-	return "someone"
-}
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
 func main() {
-	name := getName()
-	fmt.Println(name)
+	c, _ := net.Dial("tcp", "127.0.0.1:8080")
+	_, err := fmt.Fprintf(c, "GET / HTTP/1.0\r\n\r\n")
+	if err != nil {
+		return
+	}
+	s, _ := bufio.NewReader(c).ReadString('\n')
+	fmt.Println(s)
 }
