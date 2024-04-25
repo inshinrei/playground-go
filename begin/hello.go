28d0526c72020cc9106aa4db6763aa0ac4c0dfd6
tokkiumai <kiwidancebad@gmail.com>
2024-04-25 21:37:21 +0300

    n. simple localhost server

--- begin/hello.go
+++ begin/hello.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
+	"net/http"
 )
 
-func main() {
-	c, _ := net.Dial("tcp", "127.0.0.1:8080")
-	_, err := fmt.Fprintf(c, "GET / HTTP/1.0\r\n\r\n")
+func hello(res http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprint(res, "hey")
 	if err != nil {
 		return
 	}
-	s, _ := bufio.NewReader(c).ReadString('\n')
-	fmt.Println(s)
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe("localhost:4040", nil)
+	if err != nil {
+		return
+	}
 }
