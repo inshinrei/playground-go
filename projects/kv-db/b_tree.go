e534380d8564a75ccf2e299a22ccabb465ab3a17
tokkiumai <kiwidancebad@gmail.com>
2025-04-11 12:17:46 +0300

    kv-db: node append kv method

--- projects/kv-db/b_tree.go
+++ projects/kv-db/b_tree.go
@@ -46,6 +46,12 @@ func (node BNode) getPtr(idx uint16) uint64 {
 	return binary.LittleEndian.Uint64(node[pos:])
 }
 
+func (node BNode) setPtr(idx uint16, value uint64) {
+	util.Assert(idx < node.nkeys())
+	pos := 4 + 8*idx
+	binary.LittleEndian.PutUint64(node[pos:], value)
+}
+
 func (node BNode) setValue(idx uint16, value uint64) {
 	util.Assert(idx < node.nkeys())
 	pos := 4 + 8*idx
@@ -58,6 +64,12 @@ func (node BNode) getOffset(idx uint16) uint16 {
 	}
 	pos := 4 + 8*node.nkeys() + 2*(idx-1)
 	return binary.LittleEndian.Uint16(node[pos:])
+}
+
+func (node BNode) setOffset(idx uint16, offset uint16) {
+	util.Assert(idx < node.nkeys())
+	pos := 4 + 8*node.nkeys() + 2*(idx-1)
+	binary.LittleEndian.PutUint16(node[pos:], offset)
 }
 
 func (node BNode) kvPos(idx uint16) uint16 {
@@ -78,4 +90,14 @@ func (node BNode) getValue(idx uint16) []byte {
 	klen := binary.LittleEndian.Uint16(node[pos+0:])
 	vlen := binary.LittleEndian.Uint16(node[pos+2:])
 	return node[pos+4+klen:][:vlen]
+}
+
+func nodeAppendKV(new BNode, idx uint16, ptr uint64, key []byte, value []byte) {
+	new.setPtr(idx, ptr)
+	pos := new.kvPos(idx)
+	binary.LittleEndian.PutUint16(new[pos+0:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(new[pos+2:], uint16(len(value)))
+	copy(new[pos+4:], key)
+	copy(new[pos+4+uint16(len(key)):], value)
+	new.setOffset(idx+1, new.getOffset(idx)+4+uint16(len(key)+len(value)))
 }
