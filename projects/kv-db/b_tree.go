073e975a78b480bd41c7d2b9bf42f076f2460835
tokkiumai <kiwidancebad@gmail.com>
2025-04-14 20:32:31 +0300

    kv-db: leaf lookup

--- projects/kv-db/b_tree.go
+++ projects/kv-db/b_tree.go
@@ -1,6 +1,9 @@
 package kv_db
 
-import "encoding/binary"
+import (
+	"bytes"
+	"encoding/binary"
+)
 import (
 	"kv-db/util"
 )
@@ -125,6 +128,21 @@ func leafUpdate(new BNode, old BNode, idx uint16, key []byte, value []byte) {
 	nodeAppendRange(new, old, 0, 0, idx)
 	nodeAppendKV(new, idx, 0, key, value)
 	nodeAppendRange(new, old, idx+1, idx+1, old.nkeys()-(idx+1))
+}
+
+func nodeLookupLE(node BNode, key []byte) uint16 {
+	nkeys := node.nkeys()
+	var i uint16
+	for i = 0; i < nkeys; i++ {
+		cmp := bytes.Compare(node.getKey(i), key)
+		if cmp == 0 {
+			return i
+		}
+		if cmp > 0 {
+			return i - 1
+		}
+	}
+	return i - 1
 }
 
 func main() {
