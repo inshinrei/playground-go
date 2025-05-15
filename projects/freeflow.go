8e65883a373312040a29224116ad6f32cac6f72d
tokkiumai <kiwidancebad@gmail.com>
2025-05-15 12:11:17 +0300

    freeflow: common chars

--- projects/freeflow.go
+++ projects/freeflow.go
@@ -1,5 +1,15 @@
 package freeflow
 
-func main() {
+func main(words []string) {
+	cnt := make([]int, 26)
+	for i := range cnt {
+		cnt[i] = 20000
+	}
+	for _, w := range words {
+		t := make([]int, 26)
+		for _, c := range w {
+			t[c-'a']++
+		}
 
+	}
 }
