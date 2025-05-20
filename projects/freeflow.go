93cc5ce1013fd236f27ca1cc93da0573cff469c0
tokkiumai <kiwidancebad@gmail.com>
2025-05-20 21:48:29 +0300

    freeflow: maximum road importance

--- projects/freeflow.go
+++ projects/freeflow.go
@@ -1,4 +1,19 @@
-package freeflow
+package main
+
+import "sort"
 
 func main() {
+}
+
+func maximumImportance(n int, roads [][]int) (ans int64) {
+	deg := make([]int, n)
+	for _, r := range roads {
+		deg[r[0]] += 1
+		deg[r[1]] += 1
+	}
+	sort.Ints(deg)
+	for i := 0; i < n; i++ {
+		ans += int64((i + 1) * deg[i])
+	}
+	return ans
 }
