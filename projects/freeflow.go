8470e4580b0bcc32dd80312f3209f717a44236f7
tokkiumai <kiwidancebad@gmail.com>
2025-06-04 15:41:48 +0300

    rand

--- projects/freeflow.go
+++ projects/freeflow.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	if n := rand.Intn(100); n == 0 {
+		fmt.Println("random number is zero")
+	} else if n > 50 {
+		fmt.Println("random number is too big", n)
+	} else {
+		fmt.Println("random number is too big", n)
+	}
+}
