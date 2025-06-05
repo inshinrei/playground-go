95a3681586c51ae1aed0f9320c9db4ab8ef91464
tokkiumai <kiwidancebad@gmail.com>
2025-06-05 09:33:08 +0300

    goto check

--- projects/freeflow.go
+++ projects/freeflow.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func main() {
-	if n := rand.Intn(100); n == 0 {
-		fmt.Println("random number is zero")
-	} else if n > 50 {
-		fmt.Println("random number is too big", n)
-	} else {
-		fmt.Println("random number is too big", n)
+	a := 10
+	goto skip
+	b := 20
+skip:
+	c := 30
+	fmt.Println(a, b, c)
+	if c > a {
+		goto inner
 	}
-
-	for i := 0; isLess(i); i++ {
-		fmt.Println(i)
+	if a < b {
+	inner:
+		fmt.Println(a, b, c)
 	}
-}
-
-func isLess(x int) bool {
-	return x < 100
 }
