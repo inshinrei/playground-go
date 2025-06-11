d8cf95ccd9a34c94a09092aa5d3d153815e5b4a6
tokkiumai <kiwidancebad@gmail.com>
2025-06-11 21:40:06 +0300

    url-shortener: init

---
+++ projects/url-shortener /internal/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-required:"true"`
+	Timeout     time.Duration `yaml:"timeout" env-required:"true"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-required:"true"`
+}
+
+func LoadConfig() {}
