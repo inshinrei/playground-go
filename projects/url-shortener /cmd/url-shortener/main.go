298059679c27605c6d1fcc20b313db187df83cc8
tokkiumai <kiwidancebad@gmail.com>
2025-06-19 10:38:03 +0300

    url-shortener: db connection

--- projects/url-shortener /cmd/url-shortener/main.go
+++ projects/url-shortener /cmd/url-shortener/main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"github.com/joho/godotenv"
-	"log"
+	"log/slog"
+	"os"
 	"url-shortener_/internal/config"
+	"url-shortener_/internal/lib/logger/sl"
+	"url-shortener_/internal/storage/sqlite"
+)
+
+const (
+	envLocal      = "local"
+	envDev        = "development"
+	envProduction = "production"
 )
 
 func main() {
 	loadEnv()
 
 	cfg := config.LoadConfig()
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting..", slog.String("env", cfg.Env))
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("storage init failed", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 }
 
 func loadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("error loading .env file")
+		os.Exit(1)
 	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProduction:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return logger
 }
